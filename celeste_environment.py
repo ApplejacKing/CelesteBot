7979bfd469a22cdf4a2647ce9fc109f62a76866f
Applejack <applejackingisfun@gmail.com>
2021-06-07 01:53:28 +0300

    create draft class for environment

---
+++ celeste_environment.py
@@ -0,0 +1,15 @@
+class CelesteEnvironment:
+    """
+    Class responsible for game environment and processing actions into the game.
+    """
+    def reset(self):
+        """
+        should restart current level in any case.
+        """
+        pass
+
+    def step(self, action):
+        """
+        should process AI actions into the game
+        """
+        pass
