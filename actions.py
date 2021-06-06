069b18655fa8d31506ec9140ae2226576daab70e
Applejack <applejackingisfun@gmail.com>
2021-06-07 02:37:28 +0300

    fixed bug with actions. Refactoring methods in environment

--- actions.py
+++ actions.py
@@ -1,8 +1,8 @@
-from direct_keys import W, A, D, ND1, ND2
+from direct_keys import W, A, D, ND1, ND3
 
 UP = 0
 LEFT = 1
 RIGHT = 2
 JUMP = 3
 DASH = 4
-action2key = {UP: W, LEFT: A, RIGHT: D, JUMP: ND1, DASH: ND2}
+action2key = {UP: W, LEFT: A, RIGHT: D, JUMP: ND1, DASH: ND3}
--- celeste_environment.py
+++ celeste_environment.py
@@ -2,37 +2,68 @@ from direct_keys import PressKey, ReleaseKey, W, A, S, D, ND1, ND2, ND3, Esc, En
 import time
 from actions import UP, LEFT, RIGHT, JUMP, DASH, action2key
 
+SHORT = 0.1
+MEDIUM = 0.3
+LONG = 0.5
+
 class CelesteEnvironment:
     """
     Class responsible for game environment and processing actions into the game.
     """
+
     def reset(self):
         """
         should restart current level in any case.
         """
         PressKey(Esc)
-        time.sleep(0.2)
+        time.sleep(SHORT)
         ReleaseKey(Esc)
         PressKey(S)
-        time.sleep(0.2)
+        time.sleep(SHORT)
         ReleaseKey(S)
         PressKey(Enter)
-        time.sleep(0.2)
+        time.sleep(SHORT)
         ReleaseKey(Enter)
         time.sleep(4)
 
-    def step(self, action):
+    def short_action(self, action):
         """
         should process AI actions into the game
         """
         PressKey(action2key[action])
-        time.sleep(0.2)
+        time.sleep(SHORT)
         ReleaseKey(action2key[action])
+
+    def short_actions(self, actions):
+        for action in actions:
+            PressKey(action2key[action])
+        time.sleep(SHORT)
+        for action in actions:
+            ReleaseKey(action2key[action])
+
+    def medium_action(self, action):
+        """
+        should process AI actions into the game
+        """
+        PressKey(action2key[action])
+        time.sleep(MEDIUM)
+        ReleaseKey(action2key[action])
+
+    def medium_actions(self, actions):
+        for action in actions:
+            PressKey(action2key[action])
+        time.sleep(MEDIUM)
+        for action in actions:
+            ReleaseKey(action2key[action])
 
 if __name__ == "__main__":
     environment = CelesteEnvironment()
     time.sleep(3)
     environment.reset()
-    environment.step(JUMP)
-    environment.step(LEFT)
+    environment.short_action(JUMP)
+    time.sleep(LONG)
+    environment.short_actions([RIGHT, UP, DASH])
+    environment.medium_action(JUMP)
+    time.sleep(LONG)
+    environment.medium_actions([RIGHT, UP, DASH])
     environment.reset()
