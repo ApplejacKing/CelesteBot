2e85742296b802ecf12d00065199b60d5d779e17
Applejack <applejackingisfun@gmail.com>
2021-06-07 02:21:41 +0300

    create actions.py and implement step function to environment

--- celeste_environment.py
+++ celeste_environment.py
@@ -1,6 +1,6 @@
 from direct_keys import PressKey, ReleaseKey, W, A, S, D, ND1, ND2, ND3, Esc, Enter
 import time
-
+from actions import UP, LEFT, RIGHT, JUMP, DASH, action2key
 
 class CelesteEnvironment:
     """
@@ -25,8 +25,14 @@ class CelesteEnvironment:
         """
         should process AI actions into the game
         """
-        pass
+        PressKey(action2key[action])
+        time.sleep(0.2)
+        ReleaseKey(action2key[action])
 
 if __name__ == "__main__":
+    environment = CelesteEnvironment()
     time.sleep(3)
-    CelesteEnvironment().reset()
+    environment.reset()
+    environment.step(JUMP)
+    environment.step(LEFT)
+    environment.reset()
