2077c2af5908552d42d2cd4cf35e278f2833d6b1
Applejack <applejackingisfun@gmail.com>
2021-06-12 00:06:14 +0300

    create moves generator for blind bot

--- main.py
+++ main.py
@@ -3,15 +3,34 @@ from simple_scorer import SimpleScorer
 from blind_bot import BlindBot
 from action import Action
 from mask_search import MaskSearch
-from moves import UP, LEFT, RIGHT, JUMP, DASH, move2key
+from moves import UP, LEFT, RIGHT, JUMP, DASH
 import time
+import random as rand
 
+
+def move_gen(seed=None):
+	ans = []
+	rand.seed(seed)
+	start_time = 0
+	end_time = 50
+	max_actions = 12
+	moves = [UP, LEFT, RIGHT, JUMP, DASH]
+	actions = rand.randint(0, max_actions) + 1
+	for i in range(actions):
+		move = rand.choice(moves)
+		length = rand.randint(0, 20) + 1
+		st = rand.randint(0, end_time - length)
+		end = st + length
+		ans.append(Action(st, move, 1))
+		ans.append(Action(end, move, 0))
+	ans = list(sorted(ans, key=lambda x: x.start_time))
+	return ans
 
 if __name__ == '__main__':
 	env = CelesteEnvironment()
 	searcher = MaskSearch()
 	scorer = SimpleScorer(env, searcher)
-	moves = [
+	ideal_bot = [
 		Action(1, JUMP, 1),
 		Action(3, DASH, 1),
 		Action(4, JUMP, 0),
@@ -29,6 +48,9 @@ if __name__ == '__main__':
 		Action(25, UP, 0),
 		Action(25, RIGHT, 0),
 	]
+	moves = move_gen()
+	for i in range(len(moves)):
+		print(moves[i])
 	bot = BlindBot(env, moves)
 	time.sleep(2)
 	print(scorer.score(bot))
